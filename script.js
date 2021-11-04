af18f72a12adfcbbf1c4cab77626fa08796c744b
finleylee2507 <46691077+finleylee2507@users.noreply.github.com>
2021-11-04 17:06:01 -0500

    Basic features completed.

--- script.js
+++ script.js
@@ -1,52 +1,136 @@
 
-document.querySelectorAll('.image').forEach(item => {
-    item.addEventListener('click', e => {
-        expandImage(e);
+var imageId = -1; //used to keep track of the current image to display 
+var numImages=document.getElementsByClassName("image").length;
+
+const addEffects = () => {
+    document.querySelectorAll('.image').forEach(item => {
+        item.addEventListener('click', expandImage);
+
+        item.addEventListener('mouseover', mouseOverEffect);
+
+        item.addEventListener('mouseout', mouseOutEffect);
+
     });
 
-    item.addEventListener('mouseover', e => {
-        mouseOverEffect(e);
-    });
+};
 
-    item.addEventListener('mouseout',()=>{
-        mouseOutEffect();
-    });
+const removeEffects = () => {
+    document.querySelectorAll('.image').forEach(item => {
+        item.removeEventListener('click', expandImage);
 
-});
+        item.removeEventListener('mouseover', mouseOverEffect);
+
+        item.removeEventListener('mouseout', mouseOutEffect);
+
+    });
+};
 
 
 
 const expandImage = (event) => {
 
-    var expandedImage = document.getElementById("biggerImage");
-    expandedImage.src = event.target.getAttribute("src");
-    expandedImage.parentElement.style.display = "block";
+    //clear effect before opening image 
+    mouseOutEffect();
+    //remove event listeners to make background inactive 
+    removeEffects();
+
+
+    //get the id of the current image 
+    imageId = parseInt(event.target.getAttribute("id"));
+
+    
+    showImage(imageId);
+
 
 }
 
-const collapseImage = (image) => {
-    image.parentElement.style.display = "none";
+
+//given an image id, display the image 
+const showImage = (n) => {
+
+    var images = document.getElementsByClassName("image");
+   
+
+    var expandedImage = document.getElementById("modal-image");
+    expandedImage.src = images[n].getAttribute("src");
+    
+
+    //jquery stuffs
+    $(".modal-container").addClass("open"); //add class to the modal container so it will be visible 
+
+    if ($(".modal-container").hasClass("open")) {
+
+        $(".attractions-container").addClass("blur"); //add bluring effect to the main section
+    }
+
+    //prevent background from scrolling 
+    document.body.style.height = "100vh";
+    document.body.style.overflowY = "hidden";
+
+
+}
+
+
+
+//undo effects 
+const collapseImage = () => {
+
+    //restore main section interactivity 
+    addEffects();
+
+    $(".modal-container").removeClass("open");
+    $(".attractions-container").removeClass("blur");
+
+    //restore background 
+    document.body.style.height = "";
+    document.body.style.overflowY = "";
+
+
+}
+
+
+const nextImage = () => {
+
+    if(imageId+1<numImages){
+        imageId += 1;
+
+        showImage(imageId);
+    }
+    
+}
+
+const previousImage = () => {
+    if(imageId-1>=0){
+        imageId -= 1;
+        showImage(imageId);
+    }
+ 
 }
 
 const mouseOverEffect = (event) => {
 
-    var currImage=event.target;
-    currImage.style.opacity="0.75";
-    currImage.style.border="8px solid";
-    currImage.style.borderImage=" linear-gradient(45deg, red, black) 1";
+    var currImage = event.target;
+    currImage.style.opacity = "0.75";
+    currImage.style.border = "8px solid";
+    currImage.style.borderImage = " linear-gradient(45deg, red, black) 1";
 
 }
 
 //clear effect
-const mouseOutEffect=()=>{
+const mouseOutEffect = () => {
 
-    var elements=document.getElementsByClassName('image');
+    var elements = document.getElementsByClassName('image');
 
-    for(var i=0;i<elements.length;i++){
-        var element=elements[i];
-        element.style.opacity=1;
-        element.style.border="none";
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.opacity = 1;
+        element.style.border = "none";
     }
 }
+
+
+
+
+
 
 
